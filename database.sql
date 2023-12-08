62bbccf97dffea0d2afc55a1007fc8d31841efb9
Dan Labrador <thonydaniellelabrador@gmail.com>
2023-12-08 06:39:47 -0500

    docs: add database definition script template #2

---
+++ database.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS products (
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(45),
+ price DECIMAL(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  FOREIGN KEY(product_id) REFERENCES products(id),
+  quantity INT
+);
+
+CREATE TABLE IF NOT EXISTS `order` (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_name VARCHAR(45),
+  order_date DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS product_order (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  product_id INT,
+  FOREIGN KEY(product_id) REFERENCES products(id),
+  order_id INT,
+  FOREIGN KEY(order_id) REFERENCES `order`(id),
+  quantity INT
+);
